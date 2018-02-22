4de516977bc6194ffa68a9136ddaea7df81f36a5
Prakhar <prakhar.kumar91@gmail.com>
2018-02-22 20:45:54 +0530

    combined and added numbering

--- r.sh
+++ r.sh
@@ -1,4 +1,6 @@
 rm -f dump.txt
 touch dump.txt
 chmod 777 dump.txt
+time psql -d postgres -f create.sql
+time psql -d postgres -f load.sql
 time psql -d postgres -f assignment2_2015CS10667_2015CS10435.sql >> dump.txt
